e77e182d3fe5ae163846389e1e46dc7b3667b1b3
SkymelTheo <erigoni.t@gmail.com>
2025-01-29 14:41:33 +0100

    deja-fait

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import Card from './components/card.tsx';
 
 function App() {
-  return  <>
-  <body>
- <Card/>
-  </body>
-  </>
+  return (
+    <>
+      <body>
+        <Card />
+      </body>
+    </>
+  );
 }
 
 export default App;
