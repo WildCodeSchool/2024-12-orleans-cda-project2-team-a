08993618632627f8d0c3d4c3fbcdf89afbccc594
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-01-30 16:34:20 +0100

    on click sur carte avec ouverture de modale

--- src/app.jsx
+++ src/app.jsx
@@ -2,25 +2,19 @@ import { useState } from 'react';
 
 import './app.scss';
 import Favorites from './components/favorites';
-import Modal from './components/modal';
+
 import NavBar from './components/navbar';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  
 
-  const toggleModal = () => {
-    setIsOpen(!isOpen);
-  };
+  
 
-  const onClose = () => {
-    setIsOpen(false);
-  };
+  
   return (
     <div className='absolute-center-container'>
-      <Modal open={isOpen} onClose={onClose} />
-      <button className='open-modal' onClick={() => toggleModal(true)}>
-        Modal button
-      </button>
+    
+      
 
       <NavBar />
       <Favorites />
--- src/components/favorites.jsx
+++ src/components/favorites.jsx
@@ -1,77 +1,89 @@
+import { useState } from 'react';
+
+import Modal from '../components/modal';
 import '../style/favorites.scss';
 
 function Favorites() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <body className='favorite'>
       <h1>My Favorite Characters</h1>
+      <Modal open={isOpen} onClose={onClose} />
       <div className='cards'>
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Thanos</h2>
           <p> Le plus grand des méchants</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Iron Man</h2>
           <p> ddddddffff</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={ toggleModal}>
           <h2> Spider Man</h2>
           <p>fsdfsffsdfd</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={ toggleModal}>
           <h2> Spider Man</h2>
           <p>fsdfsffsdfd</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Spider Man</h2>
           <p>fsdfsffsdfd</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Spider Man</h2>
           <p>fsdfsffsdfd</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Spider Man</h2>
           <p>fsdfsffsdfd</p>
         </div>
       </div>
       <div className='cards'>
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Thanos</h2>
           <p> Le plus grand des méchants</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Iron Man</h2>
           <p> ddddddffff</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Spider Man</h2>
           <p>fsdfsffsdfd</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Spider Man</h2>
           <p>fsdfsffsdfd</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Spider Man</h2>
           <p>fsdfsffsdfd</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Spider Man</h2>
           <p>fsdfsffsdfd</p>
         </div>
 
-        <div className='carte'>
+        <div className='carte' onClick={toggleModal}>
           <h2> Spider Man</h2>
           <p>fsdfsffsdfd</p>
         </div>
