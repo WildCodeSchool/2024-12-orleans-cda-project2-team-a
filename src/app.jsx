1aedada829ce6867b75ca72abdf93b85c91cdfd4
SkymelTheo <erigoni.t@gmail.com>
2025-01-30 13:41:39 +0100

    doublon-fichier-ok

---
+++ src/app.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Card from './components/card';
+import './app.scss';
+import Modal from './components/modal';
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+  return (
+    <div className='absolute-center-container'>
+      <Modal open={isOpen} onClose={onClose} />
+      <button className='open-modal' onClick={() => toggleModal(true)}>
+        Modal button
+      </button>
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/card.scss'
 
 function Card() {
   return (
