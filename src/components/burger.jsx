0cd411b7d408ca6f05054e8a598d352d2e22321d
SkymelTheo <erigoni.t@gmail.com>
2025-01-31 11:49:27 +0100

    DIT-MOI-QUE-CEST-BON

--- src/components/burger.jsx
+++ src/components/burger.jsx
@@ -1,22 +1,20 @@
-// function Burger(){
-// return(
-//     <>
-//     <nav>
-//         <div>
-//             <ul>
-//                 <li>
-//                     <a href="#">Home</a>
-//                 </li>
-//                 <li>
-//                     <a href="#">Favorite</a>
-//                 </li>
-//                 <li>
-//                     <a href="#">Comics</a>
-//                 </li>
-//             </ul>
-//         </div>
-//     </nav>
-//     </>
+function Burger() {
+  return (
+    <>
+      <nav>
+        <div>
+          <ul>
+            <li>
+              <a href='#'>Favorite</a>
+            </li>
+            <li>
+              <a href='#'>Comics</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
 
-// );
-// }
+export default Burger;
