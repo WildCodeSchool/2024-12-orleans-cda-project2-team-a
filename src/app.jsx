4634df2aab02e15910acd2d23bd4b5372a2649d0
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-01-31 11:20:49 +0100

    maj

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,14 @@
 import './app.scss';
-
 import Favorites from './components/favorites';
 import NavBar from './components/navbar';
 import ProfilePage from './pages/profile-page';
 
-
 function App() {
   return (
-
-    <div >
+    <div>
       <NavBar />
       <Favorites />
-    <ProfilePage />
+      <ProfilePage />
     </div>
   );
 }
