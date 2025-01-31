9835c54a0e3f90b8a506b3a347f86921d1ebe2ac
SkymelTheo <erigoni.t@gmail.com>
2025-01-31 15:30:30 +0100

    DIT-MOI-QUE-CEST-BON

--- src/components/burger.jsx
+++ src/components/burger.jsx
@@ -1,20 +1,28 @@
-function Burger() {
-  return (
-    <>
-      <nav className='burger'>
-        <div>
-          <ul>
-            <li>
-              <a href='#'>Favorite</a>
-            </li>
-            <li>
-              <a href='#'>Comics</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
-}
+// import'../style/burger.scss';
+// import { useState } from 'react';
 
-export default Burger;
+// const [showLinks, setShowLinks] = useState(false)
+
+// const handleShowLinks = ()=> {setShowLinks(!showLinks)}
+
+// function Burger() {
+//   return (
+//     <>
+//       <nav className='main'>
+//         <img className="burger" onClick={handleShowLinks} src="/pictures/main-burger.png" alt="burger" />
+//         <div className='box-main'>
+//           <ul className='listing-main'>
+//             <li>
+//               <a href='#'>Favorite</a>
+//             </li>
+//             <li>
+//               <a href='#'>Comics</a>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//     </>
+//   );
+// }
+
+// export default Burger;
