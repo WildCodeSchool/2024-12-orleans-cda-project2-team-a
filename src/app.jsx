fbcf8e1c11a67320b3befa0c30070b25c9f8c5ba
sadjoaldi <alsannealdi@hotmail.com>
2025-02-04 15:32:51 +0100

    fetch

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,36 @@
+import md5 from 'md5';
+import { useEffect, useState } from 'react';
+
+import Appp from './Appp';
 import './app.scss';
-import Favorites from './components/favorites';
-import NavBar from './components/navbar';
-import ProfilePage from './pages/profile-page';
 
 function App() {
+  const [fetchData, setFetchData] = useState([]);
+
+  useEffect(() => {
+    const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+    const privateKey = import.meta.env.VITE_PRIVATE_KEY;
+    const ts = new Date().getTime();
+    const hash = md5(ts + privateKey + publicKey);
+
+    fetch(`https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`)
+      .then((res) => {
+        if (!res.ok) throw new Error('Erreur réseau');
+        return res.json();
+      })
+      .then((data) => setFetchData(data.data.results))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
-      <NavBar />
-      <Favorites />
-      <ProfilePage />
+      {fetchData.map((character) => (
+        <ul key={character.id}>
+          <li>{character.name}</li>
+        </ul>
+      ))}
+      <Appp />
+      <h1>fetch</h1>
     </>
   );
 }
