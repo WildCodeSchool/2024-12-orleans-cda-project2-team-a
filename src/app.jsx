433bd2666dd4da601abb106ef373cbd4dd5a35d2
sadjoaldi <alsannealdi@hotmail.com>
2025-02-04 15:01:03 +0100

    version mobile page profile, ajustement du style, ajout d'une variable border-radius dans le settings,

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,11 @@
-import App1 from './App1';
 import './app.scss';
 import Favorites from './components/favorites';
 import NavBar from './components/navbar';
 import ProfilePage from './pages/profile-page';
 
 function App() {
-  // useEffect(() => {
-  //   fetch('https://gateway.marvel.com/docs/public/v1/public/characters')
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data));
-  // });
-
   return (
     <>
-      <App1 />
       <NavBar />
       <Favorites />
       <ProfilePage />
