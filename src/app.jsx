4eeba69656e0a42c288450c2c2f0b04c0b5f1f77
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-02-04 11:38:18 +0100

    ajout router

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+
 import './app.scss';
 import Favorites from './components/favorites';
+import Footer from './components/footer';
+import Home from './components/home';
 import NavBar from './components/navbar';
 import ProfilePage from './pages/profile-page';
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Favorites />
-      <ProfilePage />
-    </div>
+    <Routes>
+      <Route element={<NavBar />}>
+        <Route element={<Footer />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<Favorites />} />
+          <Route path='/profilePage' element={<ProfilePage />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 import '../style/navbar.scss';
 
@@ -6,23 +7,36 @@ function Navbar() {
   const [input, setInput] = useState('');
 
   return (
-    <nav>
-      <ul>
-        <li>
-          {' '}
-          <img src='/home.png' alt='' /> Home
-        </li>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              {' '}
+              <Link to='/' className='myLink'>
+                <img src='/home.png' alt='' /> Home
+              </Link>
+            </li>
 
-        <li className='input-wrapper'>
-          <img src='/search.png' alt='' />
-          <input type='text' placeholder='Search' value={input} onChange={(e) => setInput(e.target.value)} />
-        </li>
-        <li>
-          <img src='/heart.png' alt='' />
-          Favorites
-        </li>
-      </ul>
-    </nav>
+            <li className='input-wrapper'>
+              <img src='/search.png' alt='' />
+              <input type='text' placeholder='Search' value={input} onChange={(e) => setInput(e.target.value)} />
+            </li>
+            <li>
+              <Link to='/favorites' className='myLink'>
+                {' '}
+                <img src='/heart.png' alt='' />
+                Favorites
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <body>
+        <Outlet />
+      </body>
+    </>
   );
 }
 
