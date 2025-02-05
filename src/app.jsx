06c4208bd62bffeeb6532db8b97972637619bbb6
sadjoaldi <alsannealdi@hotmail.com>
2025-02-05 20:55:32 +0100

    creation d'un composant qui contient le fetch

--- src/app.jsx
+++ src/app.jsx
@@ -1,29 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 
 import './app.scss';
-import Card from './components/card';
+import MarvelCharacters from './components/marvel-characters';
 import FavoritePage from './pages/favorite-page';
-import HomePage from './pages/home-page';
-
-const publicKey = import.meta.env.VITE_PUBLIC_KEY;
 
 function App() {
-  const [marvelCharacter, setMarvelCharacter] = useState([]);
   const [favorites, setFavorites] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://gateway.marvel.com/v1/public/characters?apikey=${publicKey}`)
-      .then((res) => {
-        if (!res.ok) throw new Error('Erreur réseau');
-        return res.json();
-      })
-      .then((data) => {
-        setMarvelCharacter(data.data.results);
-      })
-      // eslint-disable-next-line no-console
-      .catch((err) => console.error(err));
-  }, []);
 
   const addToFavorites = (char) => {
     if (!favorites.some((fav) => fav.id === char.id)) {
@@ -38,29 +21,7 @@ function App() {
         <Link to='/favorites'>Favorites</Link>
       </nav>
       <Routes>
-        <Route
-          path='/'
-          element={
-            <>
-              <HomePage />
-              {/* <ProfilePage /> */}
-              <h1>fetch</h1>
-              {marvelCharacter
-                .filter((marvel) => marvel.thumbnail && marvel.thumbnail.path && marvel.thumbnail.extension)
-                .slice(0, 20)
-                .map((character) => {
-                  return (
-                    <Card
-                      key={character.id}
-                      character={character.name}
-                      image={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                      onAddToFavorites={() => addToFavorites(character)}
-                    />
-                  );
-                })}
-            </>
-          }
-        />
+        <Route path='/' element={<MarvelCharacters addToFavorites={addToFavorites} />} />
         <Route path='/favorites' element={<FavoritePage favorites={favorites} />} />
       </Routes>
     </>
---
+++ src/components/marvel-characters.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+
+import Card from './card';
+
+const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+
+export default function MarvelCharacters({ addToFavorites }) {
+  const [marvelCharacter, setMarvelCharacter] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://gateway.marvel.com/v1/public/characters?apikey=${publicKey}`)
+      .then((res) => {
+        if (!res.ok) throw new Error('Erreur réseau');
+        return res.json();
+      })
+      .then((data) => {
+        setMarvelCharacter(data.data.results);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  const handleAddToFavorites = (character) => {
+    addToFavorites(character);
+  };
+
+  return (
+    <div>
+      <h1>Marvel Characters</h1>
+      <div className='flex'>
+        {marvelCharacter
+          .filter((marvel) => marvel.thumbnail && marvel.thumbnail.path && marvel.thumbnail.extension)
+          .slice(0, 20)
+          .map((character) => (
+            <Card
+              key={character.id}
+              character={character.name}
+              image={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+              onAddToFavorites={() => handleAddToFavorites(character)}
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
