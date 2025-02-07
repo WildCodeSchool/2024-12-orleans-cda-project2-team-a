bfab8d6fe0c527ae854513e5a9014a238441c9df
SkymelTheo <erigoni.t@gmail.com>
2025-02-07 14:18:35 +0100

    modale-profil

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,13 +1,9 @@
 import Card from '../components/card';
-import Footer from '../components/footer';
-import NavBar from '../components/navbar';
 
 export default function HomePage() {
   return (
     <>
-      <NavBar />
       <Card />
-      <Footer />
     </>
   );
 }
