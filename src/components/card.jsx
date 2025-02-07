5b7b7ef81d0367f858cf9cee5f8ca972a303a885
SkymelTheo <erigoni.t@gmail.com>
2025-02-07 14:02:29 +0100

    modale-profil

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -14,28 +14,17 @@ function Card({ character, image, onAddToFavorites }) {
   };
 
   return (
-    <div className='card'>
-      <img src={image} alt={character} onClick={handleDisplayProfil} />
-      <h2>{character}</h2>
-      <button onClick={onAddToFavorites}>
-        <img className='heart' src='/pictures/heart-cards.png' alt='' />
-      </button>
-      <Modal open={displayProfil} onClose={onClose} />
-    </div>
+    <>
+      <div className='card'>
+        <img src={image} alt={character} onClick={handleDisplayProfil} />
+        <h2>{character}</h2>
+        <button className='btn' onClick={onAddToFavorites}>
+          <img className='heart' src='/pictures/heart-cards.png' alt='' />
+        </button>
+        <Modal open={displayProfil} onClose={onClose} />
+      </div>
+    </>
   );
 }
 
 export default Card;
-
-//   <>
-
-// const [isOpen, setIsOpen] = useState(false);
-// const toggleModal = () => {
-//   setIsOpen(!isOpen);
-// };
-
-// const onClose = () => {
-//   setIsOpen(false);
-// };
-
-//   <Modal open={isOpen} onClose={onClose} />
