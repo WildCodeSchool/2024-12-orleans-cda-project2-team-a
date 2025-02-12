754d8d780f4ff9e83d4823c67c9eeeb653e25a2c
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-02-12 14:45:08 +0100

    création page 404

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import './app.scss';
 import MarvelCharacters from './components/marvel-characters';
+import Layout from './components/router-layout';
 import FavoritePage from './pages/favorite-page';
+import NotFound from './pages/not-found-page';
 
 function App() {
   const [favorites, setFavorites] = useState([]);
@@ -16,13 +18,13 @@ function App() {
 
   return (
     <>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/favorites'>Favorites</Link>
-      </nav>
       <Routes>
-        <Route path='/' element={<MarvelCharacters addToFavorites={addToFavorites} />} />
-        <Route path='/favorites' element={<FavoritePage favorites={favorites} />} />
+        <Route element={<Layout />}>
+          {/* <Route path='/' element={<Home />} /> */}
+          <Route path='/' element={<MarvelCharacters addToFavorites={addToFavorites} />} />
+          <Route path='/favorites' element={<FavoritePage favorites={favorites} />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
       </Routes>
     </>
   );
