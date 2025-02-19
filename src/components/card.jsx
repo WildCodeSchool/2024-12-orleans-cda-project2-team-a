c1b0279e8b0b3a5ad81300b63ef237c1a226b4e7
sadjoaldi <alsannealdi@hotmail.com>
2025-02-19 10:51:25 +0100

    creation fichier style, correction conflict

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,16 +4,20 @@ export default function Card({ character, image, onAddToFavorites, onClick }) {
   return (
     <div className='card' onClick={onClick}>
       <img src={image} alt={character} className='heart' />
-      <h3>{character}</h3>
-
-      <button
-        onClick={(e) => {
-          e.stopPropagation();
-          onAddToFavorites();
-        }}
-      >
-        😎
-      </button>
+      <div className='space'>
+        <button
+          className='btn'
+          onClick={(e) => {
+            e.stopPropagation();
+            onAddToFavorites();
+          }}
+        >
+          <img className='heart' src='/pictures/heart-cards.png' alt='' />
+        </button>
+      </div>
+      <div className='character-title'>
+        <h2>{character}</h2>
+      </div>
     </div>
   );
 }
