07429a090e3c8589dbebe06117791f22be5f942c
sadjoaldi <alsannealdi@hotmail.com>
2025-02-27 10:47:10 +0100

    pull release encore

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,22 +3,20 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import '../style/card.scss';
 
-  const notify = () => {
-    toast.success('🎉 Added to Favorites !', {
-      position: 'top-right',
-      autoClose: 3000,
-      hideProgressBar: false,
+const notify = () => {
+  toast.success('🎉 Added to Favorites !', {
+    position: 'top-right',
+    autoClose: 3000,
+    hideProgressBar: false,
 
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      theme: 'light',
-    });
-  };
-
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    theme: 'light',
+  });
+};
 
 export default function Card({ character, image, onAddToFavorites, onClick, onRemove, isFavoritePage }) {
-
   return (
     <div className='card' onClick={onClick}>
       <img src={image} alt={character} className='heart' />
@@ -47,7 +45,6 @@ export default function Card({ character, image, onAddToFavorites, onClick, onRe
             <img className='remove-fav' src='/pictures/remove-fave.svg' alt='' />
           </button>
         )}
-
       </div>
       <div className='character-title'>
         <h2>{character}</h2>
